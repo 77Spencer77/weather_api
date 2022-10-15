3ac9451a587f02724577f09cabfc7e67167f2554
Arunangshu Roy <89198409+77Spencer77@users.noreply.github.com>
2022-10-16 04:15:53 +0530

    Update api.js

--- api.js
+++ api.js
@@ -1,17 +1,25 @@
-let dest=document.getElementById("input_text");
-let button=document.getElementById("button");
-// let name1=document.getElementById("name");
-let temp=document.getElementById("temp");
-let desc=document.getElementById("desc");
-button.addEventListener("click",function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q='+dest.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
-    .then(jay=>jay.json())
-    .then(abhinav => 
-        {
-        var temp_=abhinav['main']['temp'];
-        var desc_=abhinav['weather'][0]['description'];
-        temp.innerHTML=temp_;
-        desc.innerHTML=desc_;
-    })
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#name');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
 
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = "Desc - "+descValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("Wrong city name!"));
 })
